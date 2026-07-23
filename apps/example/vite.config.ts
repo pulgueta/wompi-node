02b1746a1a944a44eb2390179416e9056dac7acd
pulgueta <retardix456@gmail.com>
2026-07-23 14:21:30 -0500

    chore: add nitro for vercel deployments

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), viteReact()],
+  plugins: [tanstackStart(), viteReact(), nitro()],
 });
 
 export default config;
